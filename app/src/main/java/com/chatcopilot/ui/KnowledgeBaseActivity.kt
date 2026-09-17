cbcc53c4ed0df15a08fe164f09f28e733ea1f8a9
amazingwork666-create <amazingwork666@gmail.com>
2026-09-17 14:33:46 +0300

    Update KnowledgeBaseActivity.kt

--- app/src/main/java/com/chatcopilot/ui/KnowledgeBaseActivity.kt
+++ app/src/main/java/com/chatcopilot/ui/KnowledgeBaseActivity.kt
@@ -1,1 +1,11 @@
+package com.chatcopilot.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class KnowledgeBaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
 
